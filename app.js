2f59816d67dbc2f035ee4f61c56d2b4664b9848b
ranyacharef <53267653+ranyacharef@users.noreply.github.com>
2022-12-09 16:18:06 +0100

    v5.0

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/blockchain');
+var routes = require('./routes/index');
 
 var app = express();
 
